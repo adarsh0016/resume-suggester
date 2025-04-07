f61a8c0149c8489bcbac1208bfc169845318fde6
Adarsh <69260888+adarsh0016@users.noreply.github.com>
2025-04-07 18:28:08 +0530

    Update suggestionService.java

--- src/main/java/com/adarsh/resumed/service/suggestionService.java
+++ src/main/java/com/adarsh/resumed/service/suggestionService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.util.Objects;
