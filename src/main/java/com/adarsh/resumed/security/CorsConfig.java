7145c73f853334e72a2eedf73ca5e87e787702c3
Adarsh <69260888+adarsh0016@users.noreply.github.com>
2025-04-07 18:48:16 +0530

    Update CorsConfig.java

--- src/main/java/com/adarsh/resumed/security/CorsConfig.java
+++ src/main/java/com/adarsh/resumed/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of("https://adarsh0016.github.io/resume-suggester-ui/")); // Or "http://localhost:3000" etc.
+        config.setAllowedOriginPatterns(List.of("https://adarsh0016.github.io")); // Or "http://localhost:3000" etc.
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
